f237a4f06c5835770a240cf8be766e813d3d2ffd
William Gomes <williamlimagomes62@gmail.com>
2021-09-06 13:41:31 -0300

    Project clean

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,27 +7,9 @@ import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de vendas</h1>
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secundary">Taxa de sucesso (%)</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secundary">Todas vendas</h5>
-            <DonutChart />
-          </div>
-        </div>
-        <div className="py-3">
-          <h2 className="text-primary">Todas vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <div>
+      <h1>Olá mundo!</h1>
+    </div>
   );
 }
 
