548863127b14a5ea1831ac9bb9c30f3fe3611f0c
William Gomes <williamlimagomes62@gmail.com>
2021-09-10 10:10:36 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,20 +11,24 @@ type ChartData = {
 
 const DonutChart = () => {
 
-  // forma errada
-  let chartData : ChartData = {labels: [], series: []};
+  const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-  // Assincrono
-  // cria as listas separadas de labels e series
-  axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then(response => {
-      const data = response.data as SaleSum[];
-      const myLabels = data.map(x => x.sellerName);
-      const mySeries = data.map(x => x.sum);
+  // Para ser executado uma unica vez
+  // quando mudar o estado das variaveis observadas, ele executa mais uma vez
+  useEffect(() => {
+    // Assincrono e deve ser feita a chamada da API uma unica vez
+    // cria as listas separadas de labels e series
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+      .then(response => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
 
-      chartData = { labels : myLabels, series : mySeries }
-      console.log(chartData)
-    })
+        setChartData({ labels: myLabels, series: mySeries });
+      });
+  }, []);
+
+
 
   const options = {
     legend: {
