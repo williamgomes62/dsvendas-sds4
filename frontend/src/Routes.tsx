a3828c39381ff9cdf65882e8f6f7e9b75f240730
William Gomes <williamlimagomes62@gmail.com>
2021-09-10 08:24:52 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,20 @@
+import Dashboard from "Pages/Dashboard";
+import Home from "Pages/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes;
