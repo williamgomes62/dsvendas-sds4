58c0d242e8e648e78308098f2a3e0f1212efd25f
William Gomes <williamlimagomes62@gmail.com>
2021-09-11 09:41:45 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
+// Se rodando localmente, pega o backend do local host
+// Se rodando na nuvem, pega o endereco da variavel de ambiente configurada
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
